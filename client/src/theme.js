1b785e1131cce24dddd90c1589f4049e6413ff90
vijay2904 <vijayrohit2904@gmail.com>
2025-05-31 12:30:43 +0530

    Added theme toggle to entire application

---
+++ client/src/theme.js
@@ -0,0 +1,15 @@
+export const lightTheme = {
+    '--bg-primary': '#8996a3',
+    '--bg-secondary': '#f0f0f0',
+    '--text-color': '#000',
+    '--primary-color': '#a19595',
+    '--bg-hr': '#000'
+};
+
+export const darkTheme = {
+    '--bg-primary': '#0e0c16',
+    '--bg-secondary': '#12101b',
+    '--text-color': '#fff',
+    '--primary-color': '#8996a3',
+    '--bg-hr': '#ddd'
+};
